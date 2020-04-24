f1b093d18edf708e9b3b56dc0e95eb4c242b457d
rakib-09 <rakib.rumi09@gmail.com>
2020-04-24 19:06:24 +0600

    added first page

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+   return( 
+   <div className="jumboTron">
+        <h1>Plural-sight Administration</h1>
+        <p>React, Flux, React router for exclusive web application</p>
+    </div>
+   )  
+}
+
+
+export default HomePage
