9e0a151d0abb6e9465cad5f17cc222d5b86bb495
rakib-09 <rakib.rumi09@gmail.com>
2020-04-27 10:17:56 +0600

    completed creating reusable text input

--- src/components/AddNewCourse.js
+++ src/components/AddNewCourse.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CourseForm from './CourseForm'
 
 const AddNewCourse = props => {
+    const [course, setCourse] = useState({
+        id: null,
+        title: null,
+        authorId: null,
+        slug: "",
+        category: null
+    })
+    
+    const handleChange = ({target}) => {
+        setCourse({
+          ...course,
+          [target.name]: target.value,
+        });
+    }
+
     return (
-        <>
+      <>
         <h2>Add Course</h2>
-        </>
-    )
+        <CourseForm course={course} onChange={handleChange} />
+      </>
+    );
 }
 
 export default AddNewCourse
--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,49 +1,40 @@
 import React from "react";
+import TextInput from "./commons/TextInput";
 
 function CourseForm(props) {
   return (
     <form>
-      <div className="form-group">
-        <label htmlFor="title">Title</label>
-        <div className="field">
-          <input
+          <TextInput
             id="title"
-            type="text"
             name="title"
-            className="form-control"
-            value=""
+            label="Title"
+            onChange={props.onChange}
+            value={props.course.title || ""}
           />
-        </div>
-      </div>
 
-      <div className="form-group">
-        <label htmlFor="author">Author</label>
-        <div className="field">
-          <select
-            id="author"
-            name="authorId"
-            value=""
-            className="form-control"
-          >
-            <option value="" />
-            <option value="1">Cory House</option>
-            <option value="2">Scott Allen</option>
-          </select>
+        <div className="form-group">
+            <label htmlFor="author">Author</label>
+            <div className="field">
+            <select
+                id="author"
+                name="authorId"
+                value={props.course.authorId || ""}
+                onChange={props.onChange}
+                className="form-control"
+            >
+                <option value="" />
+                <option value="1">Cory House</option>
+                <option value="2">Scott Allen</option>
+            </select>
+            </div>
         </div>
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="category">Category</label>
-        <div className="field">
-          <input
-            type="text"
+          <TextInput
             id="category"
             name="category"
-            className="form-control"
-            value=""
+            label="Category"
+            onChange={props.onChange}
+            value={props.course.category || ""}
           />
-        </div>
-      </div>
 
       <input type="submit" value="Save" className="btn btn-primary" />
     </form>
